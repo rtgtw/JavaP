96cdcbd9646bb37301d1aab253b14dda2e0c7f7e
Remi Olateru <remiolateru@gmail.com>
2024-09-03 11:17:11 -0400

    Using ByteArrayStream

--- src/Io/InputOutput.java
+++ src/Io/InputOutput.java
@@ -1,5 +1,49 @@
 package Io;
+import java.io.*;
 
 
+//You can turn a byte array into a string
 public class InputOutput {
+
+    public static void main(String args[]) throws Exception {
+
+
+        //Byte output stream creates a byte array
+        ByteArrayOutputStream bos = new ByteArrayOutputStream(20);
+
+        bos.write('a');
+        bos.write('b');
+        bos.write('c');
+        bos.write('d');
+
+        bos.writeTo(new FileOutputStream("C:\\Users\\remio\\OneDrive\\Desktop\\testing123.txt"));
+        System.out.println("Success");
+
+//        byte b[] = bos.toByteArray();
+//
+//        for(byte x : b){
+//            System.out.println( (char)x);
+//        }
+
+        bos.close();
+
+
+
+
+
+
+
+//        //Byte array input stream
+//        byte b[] = {'a', 'b', 'c', 'd',' '};
+//
+//        ByteArrayInputStream bis = new ByteArrayInputStream(b);
+//
+//        String str = new String(bis.readAllBytes());
+//
+//        System.out.println(str);
+//
+//        bis.close();
+
+
+    }
 }
